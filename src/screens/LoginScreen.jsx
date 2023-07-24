2836fe8c75cba469a9f1920c5a38c0fa4882a580
carrie-owo <55964935+carrie-owo@users.noreply.github.com>
2023-07-24 16:30:31 -0700

    add query parameter to login screen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -2,8 +2,13 @@ import GoogleAuth from "../components/GoogleAuth"
 import { useLocation } from "react-router-dom"
 
 const LoginScreen = () => {
-    const location = useLocation()
-    const { contentUrl } = location.state
+    // const location = useLocation()
+    // const { contentUrl } = location.state
+
+    const queryParams = new URLSearchParams(window.location.search)
+    const contentUrl = queryParams.get("contentUrl")
+
+
     return (
         <div className="container">
             <h1 className="title">Get your podcast via email</h1>
