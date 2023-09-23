426b55e6985afd7e699c5050a075ad8b04415d64
Anni Chen <anni.chen@duke.edu>
2023-09-23 10:28:12 -0700

    Add podcast script edit functionality

--- src/screens/RouterScreen.jsx
+++ src/screens/RouterScreen.jsx
@@ -1,39 +1,46 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import LoginScreen from './LoginScreen';
-import ResultScreen from './ResultScreen';
-import { initializeFirebaseApp } from '../util/firebaseUtils';
-import { getAuth, onAuthStateChanged } from '@firebase/auth';
-import DashBoardScreen from './DashBoardScreen';
-import { useAppDispatch } from '../redux/hooks';
-import { setUserEmail, setUserId, setUserProfilePic, setUserDisplayName, setUserIdToken } from '../redux/userSlice';
-import WebFont from 'webfontloader'
-import SubscriptionScreen from './SubscriptionScreen';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import LoginScreen from "./LoginScreen";
+import ResultScreen from "./ResultScreen";
+import { initializeFirebaseApp } from "../util/firebaseUtils";
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
+import DashBoardScreen from "./DashBoardScreen";
+import { useAppDispatch } from "../redux/hooks";
+import {
+  setUserEmail,
+  setUserId,
+  setUserProfilePic,
+  setUserDisplayName,
+  setUserIdToken,
+} from "../redux/userSlice";
+import WebFont from "webfontloader";
+import SubscriptionScreen from "./SubscriptionScreen";
+import PodcastEditScreen from "./PodcastEditScreen";
 
 const RouterScreen = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-      WebFont.load({
-          google: {
-              families: ['Poppins:400,500,700'],
-          },
-      })
-  }, [])
+    WebFont.load({
+      google: {
+        families: ["Poppins:400,500,700"],
+      },
+    });
+  }, []);
 
   useEffect(() => {
-    const app = initializeFirebaseApp()
-    const auth = getAuth(app)
-    onAuthStateChanged(auth, user => {
+    const app = initializeFirebaseApp();
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, (user) => {
       if (user) {
-        dispatch(setUserId(user.uid))
-        dispatch(setUserEmail(user.email))
-        dispatch(setUserProfilePic(user.photoURL))
-        dispatch(setUserDisplayName(user.displayName))
-        dispatch(setUserIdToken(user.accessToken))
+        dispatch(setUserId(user.uid));
+        dispatch(setUserEmail(user.email));
+        dispatch(setUserProfilePic(user.photoURL));
+        dispatch(setUserDisplayName(user.displayName));
+        dispatch(setUserIdToken(user.accessToken));
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <BrowserRouter>
@@ -41,7 +48,8 @@ const RouterScreen = () => {
         <Route path="/" element={<DashBoardScreen />} />
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/dashboard" element={<DashBoardScreen />} />
-        <Route path="/result" element={<ResultScreen />}/>
+        <Route path="/result" element={<ResultScreen />} />
+        <Route path="/edit" element={<PodcastEditScreen />} />
         <Route path="/subscription" element={<SubscriptionScreen />} />
       </Routes>
     </BrowserRouter>
